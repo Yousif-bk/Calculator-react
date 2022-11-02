a2ba5bed1661590263970e620c6fb5970f91ab29
Yousif <babikery71@gmail.com>
2022-11-02 16:47:27 +0400

    [fix] fixed some bugs

--- src/components/AddDeleteRow.js
+++ src/components/AddDeleteRow.js
@@ -1,45 +1,50 @@
 import React, { useState } from "react";
 
-function AddDeleteRow({ rowsData, deleteRows,  handleChange }) {
+function AddDeleteRow({ rowsData, deleteRows, handleChange }) {
   // const [disabled, setDisabled] = useState(false)
 
-  const disableRow = (index, evnt)=> {
-    console.log(index)
+  // Disable Specific Row
+  const disableRow = (index, evnt) => {
     const { name, value } = evnt.target;
     const rowsInput = [...rowsData];
     rowsInput[index][name] = value;
-
-    rowsInput[index].disabled = true
-
-    // setDisabled(true)
-  }
+    console.log(rowsInput);
+    rowsInput[index].disabled = true;
+    //  setDisabled(true)
+  };
 
   return rowsData.map((data, index) => {
-    const { numValue,calculation, disabled } = data;
+    const { numValue, calculation, disabled } = data;
     return (
       <ul key={index}>
         <li className="d-flex mb-3">
+
           <select
-           disabled = {disabled}
-           value={calculation} name="calculation" onChange={(evnt) => handleChange(index, evnt)}>
+            disabled={disabled}
+            value={calculation}
+            name="calculation"
+            onChange={(evnt) => handleChange(index, evnt)}>
             <option value={true}>+</option>
             <option value={false}>-</option>
           </select>
+
           <input
-            disabled = {disabled}
+            disabled={disabled}
             type="text"
             className="form-control w-25 mx-2"
             name="numValue"
             value={numValue}
-            onChange={(evnt) => handleChange(index, evnt)}
-          />
+            onChange={(evnt) => handleChange(index, evnt)}/>
+
+          {/* Delete btn */}
           <button
-           disabled = {disabled}
+            disabled={disabled}
             className="btn btn-danger mx-2"
             onClick={() => deleteRows(index)}>
             Delete
           </button>
 
+          {/*  Disable Btn */}
           <button
             className="btn btn-light mx-2"
             onClick={(evnt) => disableRow(index, evnt)}>
--- src/components/DynamicRow.js
+++ src/components/DynamicRow.js
@@ -2,64 +2,55 @@ import React from "react";
 import { useState } from "react";
 import AddDeleteRow from "./AddDeleteRow";
 function DynamicRow() {
-
   // initial state
-  const [rowsData, setRowsData] = useState([]);
+  const initialState =  { numValue: "", calculation: "true", disabled: false }
+  const [rowsData, setRowsData] = useState([initialState]);
   const [result, setResult] = useState(0);
 
-
-
-// calculation
+  // calculation inputs
   const calculation = (rowsInput) => {
     let sum;
-    array.reduce((accumulator, obj) => {
-      if(accumulator == undefined && obj.calculation == "false"){
-        setResult(result - Number(array[0].numValue));
-        // setResult(sum)
-      }
-      else if (accumulator !== undefined && obj.calculation == "true") {
+    rowsInput.reduce((accumulator, obj) => {
+      if (accumulator == undefined && obj.calculation == "false") {
+        setResult(result - Number(rowsInput[0].numValue));
+      } else if (accumulator !== undefined && obj.calculation == "true") {
         sum = accumulator + Number(obj.numValue);
         setResult(sum);
-      } 
+      }
       return sum;
     }, 0);
-    
   };
 
-  // Add Row
+  // Add New Row
   const addRows = () => {
     const rowsInput = {
       numValue: "",
       calculation: "true",
-      disabled: false
+      disabled: false,
     };
     setRowsData([...rowsData, rowsInput]);
   };
 
- 
-  // handle Change
+  // handle Input Value Change
   const handleChange = (index, evnt) => {
     evnt.preventDefault();
     const { name, value } = evnt.target;
     const rowsInput = [...rowsData];
     rowsInput[index][name] = value;
-    // console.log(rowsInput, "rowsInput");
 
     // Call calculation method
     calculation(rowsInput);
-
     setRowsData(rowsInput);
   };
 
-  // Delete Rows
+  // Delete Row
   const deleteRows = (index) => {
     const rows = [...rowsData];
     rows.splice(index, 1);
     setRowsData(rows);
     // Call calculation method
-    calculation(rows)
+    calculation(rows);
   };
-
 
   return (
     <div className="container mt-5">
